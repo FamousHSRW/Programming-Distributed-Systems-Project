01c31f4ce9637f9015c99f501ba90f2d6cc50675
Nnange <47540685+Nnange@users.noreply.github.com>
2019-11-22 15:47:44 +0100

    register-ask-name-password

--- src/com/programming_distributed_systems_project/User.java
+++ src/com/programming_distributed_systems_project/User.java
@@ -6,31 +6,42 @@ public class User {
     private static String username;
     private  static String password;
     public static void login() {
+        getUser();
+    }
+
+    public static void register() {
+        getUser();
+    }
+
+    public static void getUser(){
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Enter your username: ");
             username = scanner.next();
-            if(isValidUserNameOrPassword(username)) {
-                System.out.println("Enter your password: ");
-                password = scanner.next();
-                if(isValidUserNameOrPassword(password)) {
-                    // server should do login
-                    break;
-                } else {
-                    printInvalidUsernameOrPassword("password");
-                }
+            if (isValidUserNameOrPassword(username)) {
+                break;
             } else {
                 printInvalidUsernameOrPassword("username");
             }
+        }
 
+        while(true) {
+            System.out.println("Enter your password: ");
+            password = scanner.next();
+            if (isValidUserNameOrPassword(password)) {
+                // server should do login
+                break;
+            } else {
+                printInvalidUsernameOrPassword("password");
+            }
         }
     }
+
     public static void printInvalidUsernameOrPassword(String argument) {
         System.out.println(argument + " must be at least 4 characters long");
     }
-    public static void register() {
-        System.out.println("Implementation of register coming soon!!");
-    }
+
     public static boolean isValidUserNameOrPassword(String argument) {
         if(argument.length() >= 4) {
             return true;
